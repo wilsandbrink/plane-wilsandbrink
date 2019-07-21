5766053c14350f249712f8743ea5d93c8143e574
William Sandbrink <wilsandbrink@gmail.com>
2019-07-21 11:09:08 +0200

    convert indentation to spaces

--- index.php
+++ index.php
@@ -7,28 +7,28 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
 
-		<?php
-		if ( have_posts() ) {
+        <?php
+        if ( have_posts() ) {
 
-			while ( have_posts() ) {
-				the_post();
-				get_template_part( 'template-parts/content/content' );
-			}
+            while ( have_posts() ) {
+                the_post();
+                get_template_part( 'template-parts/content/content' );
+            }
 
-			plane_content_nav( 'nav-below' );
+            plane_content_nav( 'nav-below' );
 
-		} else {
+        } else {
 
-			get_template_part( 'template-parts/content/content', 'none' );
+            get_template_part( 'template-parts/content/content', 'none' );
 
-		}
-		?>
+        }
+        ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+        </main><!-- #main -->
+    </div><!-- #primary -->
 
 <?php get_sidebar(); ?>
 
